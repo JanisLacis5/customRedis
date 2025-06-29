beae6be991c3c0aed7f0a52bdf44f3aeea0bc39c
janis <janislacis06@gmail.com>
2025-06-29 23:09:19 +0300

    create insert function for avl tree

--- tests/test_avl.cpp
+++ tests/test_avl.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <assert.h>
+#include <cstdio>
+
 #include "../avl_tree.h"
 
 #define container_of(ptr, T, member) ((T *)( (char *)ptr - offsetof(T, member) ))
@@ -13,9 +15,9 @@ struct Container {
     AVLNode *root = NULL;
 };
 
-static void avl_verify(AVLNode *parent, AVLNode *node) {
+static int avl_verify(AVLNode *parent, AVLNode *node) {
     if (!node) {
-        return;
+        return 0;
     }
 
     // verify parent
@@ -42,14 +44,38 @@ static void avl_verify(AVLNode *parent, AVLNode *node) {
         uint32_t r_val = container_of(node->right, Data, node)->data;
         assert(r_val > val);
     }
+    return 0;
 }
 
-// static void add() {
-//
-// }
+static void add(Container &c, const uint32_t &val) {
+    // Create data object
+    Data *data = new Data();
+    avl_init(&data->node);
+    data->data = val;
+
+    // Insert the node in the tree
+    AVLNode **from = &c.root;
+    AVLNode *curr = NULL;
+    while (*from) {
+        curr = *from;
+        uint32_t cval = container_of(curr, Data, node)->data;
+        from = cval < data->data ? &curr->left : &curr->right;
+    }
+    *from = &data->node;
+    data->node.parent = curr;
+    avl_fix(&data->node);
+}
 
 int main() {
     Container c;
-    avl_verify(NULL, c.root);
-    assert(c.root->size == 0);
+
+    // Add and then remove a value to test an empty tree
+    add(c, 1);
+    int err = avl_verify(NULL, c.root);
+    if (err) {
+        printf("Error no 1\n");
+        return -1;
+    }
+
+    printf("All tests passed!\n");
 }
