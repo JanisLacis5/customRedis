ec687a43885d577df52c70852f48624d4facbb81
janis <janislacis06@gmail.com>
2025-08-14 16:33:36 +0300

    create some hll functions

--- data_structures/hyperloglog.cpp
+++ data_structures/hyperloglog.cpp
@@ -1,13 +1,36 @@
 #include "hyperloglog.h"
 
-uint8_t pf_add(std::string *hll, std::string &val) {
+#include <string.h>
+
+static bool verify_hll(std::string &hll) {
+    return hll.size() == (HLL_DENSE_SIZE_BYTES + HLL_HEADER_SIZE_BYTES) && hll.substr(0, 4) == "HYLL";
+}
+
+static HLLHeader* new_hdr(uint8_t encoding) {
+    HLLHeader *hdr = new HLLHeader();
+    hdr->magic[0] = 'H';
+    hdr->magic[1] = 'Y';
+    hdr->magic[2] = 'L';
+    hdr->magic[3] = 'L';
+    hdr->enc = encoding;
+    return hdr;
+}
+
+void hll_init(std::string &hll) {
+    // Create the header
+    // TODO: use sparse type as well
+    HLLHeader *hdr = new_hdr(HLL_DENSE);
+}
+
+
+uint8_t hll_add(std::string *hll, std::string &val) {
 
 }
 
-uint64_t pf_count(std::string *hll) {
+uint64_t hll_count(std::string *hll) {
 
 }
 
-void pf_merge(std::string *hll1, std::string *hll2) {
+void hll_merge(std::string *hll1, std::string *hll2) {
 
 }
--- data_structures/hyperloglog.h
+++ data_structures/hyperloglog.h
@@ -3,11 +3,23 @@
 #include <stdint.h>
 #include <string>
 
-const int32_t HLL_SIZE_BITS = 16 * 8 + 6 * 16384;
+const int32_t HLL_HEADER_SIZE_BYTES = 16;
+const int32_t HLL_DENSE_SIZE_BYTES = 6 * 16384 / 8; // 2^14 (16384) 6 bit registers
+enum HLLEncoding {
+    HLL_DENSE = 0,
+    HLL_SPARSE = 1
+};
 
-uint8_t pf_add(std::string &hll, std::string &val);
-uint64_t pf_count(std::string &hll);
-void pf_merge(std::string &hll1, std::string &hll2);
+struct HLLHeader {
+    uint8_t magic[4];
+    uint8_t enc;
+    uint8_t unused[3];
+    uint8_t card[8];
+};
+
+uint8_t hll_add(std::string &hll, std::string &val);
+uint64_t hll_count(std::string &hll);
+void hll_merge(std::string &hll1, std::string &hll2);
 
 
 #endif
